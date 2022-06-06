c2f901ee4b5d460e644c97fc7a2190984d9d830e
jacoposergio <djago32@gmail.com>
2022-06-06 17:14:47 +0200

    tasto cancel

--- js/script.js
+++ js/script.js
@@ -9,14 +9,30 @@ generateButton.addEventListener('click',
             console.log(userAge)
 
             const price = userKm * 0.21;
-            const discount = 0;
+            let discount = 0;
 
             if(userAge === 'minorenne') {
                 discount = price * 20 / 100;
             } else if (userAge === 'over') {
-            discount = price * 40 / 100;
+               discount = price * 40 / 100;
         }
 
-        const finalPrice = price - discount;
-        console.log(fina 88)
+        const finalPrice = (price - discount).toFixed(2);
+        console.log(finalPrice);
+
+        document.getElementById('ticket_name').innerHTML = userName;
+        document.getElementById('ticket').classList.add('active');
+    }
+);
+
+const cancelButton = document.getElementById('cancel_ticket');
+generateButton.addEventListener('click',
+        function() {
+            document.getElementById('user_name').value ='';
+            document.getElementById('user_km').value ='';
+            document.getElementById('user_age').value ='maggiorenne';
+        }
+
+        document.getElementById('ticket_name').innerHTML = '';
+        // document.getElementById('ticket').classList.add('active');
 );
