6cc85da377e8d58b9c7cd045a338a176e5664492
jacoposergio <djago32@gmail.com>
2022-06-06 19:08:49 +0200

    finito

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// BOTTONE GENERA
+
 const generateButton = document.getElementById('generate_ticket');
 generateButton.addEventListener('click',
         function() {
@@ -17,15 +19,41 @@ generateButton.addEventListener('click',
                discount = price * 40 / 100;
         }
 
+        // calcolo prezzo finale 
+
         const finalPrice = (price - discount).toFixed(2);
         console.log(finalPrice);
 
+        /*offerta biglietto: per over 18 sarà standard,
+        minorenne sarà under, over 65 sarà over */ 
+
+        let ticketOffer = 'Standard'
+
+        if(userAge === 'minorenne') {
+            ticketOffer = 'Sconto Under';
+        } else if(userAge === 'over') {
+            ticketOffer = 'Sconto Over';
+        }
+
+        // numero del vagone
+
+        trainCar = Math.floor((Math.random() * 10) + 1);
+
+           // codice del biglietto
+
+           coCode = Math.random().toString(36).slice(2)
+
+       // dati del biglietto generato 
         document.getElementById('ticket_name').innerHTML = userName;
         document.getElementById('ticket_price').innerHTML = finalPrice;
-        document.getElementById('ticket_age').innerHTML = userAge;
+        document.getElementById('ticket_age').innerHTML = ticketOffer;
+        document.getElementById('train_car').innerHTML = trainCar;
+        document.getElementById('co_code').innerHTML = coCode;
         document.getElementById('ticket').classList.add('active');
     }
 );
+
+// BOTTONE CANCELLA 
 
 const cancelButton = document.getElementById('cancel_ticket');
 cancelButton.addEventListener('click',
@@ -34,10 +62,12 @@ cancelButton.addEventListener('click',
             document.getElementById('user_km').value ='';
             document.getElementById('user_age').value ='maggiorenne';
         
-
-        document.getElementById('ticket_name').innerHTML = '';
-        document.getElementById('ticket_age').innerHTML = '';
-        document.getElementById('ticket_price').innerHTML = '';
-        document.getElementById('ticket').classList.toggle('active');
+// dati del biglietto generato 
+        document.getElementById('ticket_name').innerHTML = 'Inserire nome';
+        document.getElementById('ticket_age').innerHTML = "Inserire fascia d'età";
+        document.getElementById('ticket_price').innerHTML = '__';
+        document.getElementById('train_car').innerHTML = '__';
+        document.getElementById('co_code').innerHTML = '__';
+        document.getElementById('ticket').classList.remove('active');
     }
 );
